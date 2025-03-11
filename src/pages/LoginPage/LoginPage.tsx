875ca48b8436ce2b57509c3ec5e0abd61e82d49a
Ivek <iradovic01@yahoo.com>
2025-03-11 21:11:21 +0100

    Adding new style

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -44,13 +44,45 @@ const LoginPage = () => {
         <div
           className="position-absolute p-4"
           style={{
-            left: "10%",
-            top: "20%",
-            backgroundColor: "rgba(255, 255, 255, 0.8)",
-            borderRadius: "8px",
+            left: "5%",
+            top: "10%",
             width: "300px",
+            background: "rgb(83 118 168 / 55%)",
+            backdropFilter: "blur(8px)",
+            borderRadius: "15px",
+            border: "1px solid rgba(255, 255, 255, 0.2)",
+            fontFamily: "'Poppins', sans-serif",
+            boxShadow: "0 8px 32px rgba(0, 0, 0, 0.1)",
           }}
-        ></div>
+        >
+          <h1
+            style={{
+              color: "#fff",
+              fontSize: "2.5rem",
+              fontWeight: 700,
+              textShadow: "2px 2px 4px rgba(0, 0, 0, 0.3)",
+              marginBottom: "1rem",
+              lineHeight: 1.2,
+              letterSpacing: "-0.5px",
+            }}
+          >
+            Monkey Business Just Got Wilder
+          </h1>
+          <p
+            style={{
+              color: "rgba(255, 255, 255, 0.9)",
+              fontSize: "1.1rem",
+              lineHeight: 1.5,
+              textShadow: "1px 1px 2px rgba(0, 0, 0, 0.2)",
+              fontWeight: 300,
+            }}
+          >
+            Join the primate party where slots are wilder than a rainforest
+            storm—bet, spin, and snag vine-swinging jackpots!
+          </p>
+
+          <div>FOrma ce ici ovdeeee</div>
+        </div>
       </div>
     </div>
   );
